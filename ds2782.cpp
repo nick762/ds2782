62f29b3adfd03a3dd81ada1078ea62039b275f73
nick762 <nzhdanov@gmail.com>
2020-11-16 14:09:27 +0300

    Поправлено чтение потребления

--- ds2782.cpp
+++ ds2782.cpp
@@ -14,14 +14,24 @@ int DS2782::_readVoltage(){
 }
 
 int DS2782::_readCurrent(){
-  int8_t registerValue;
+  int sense_res;
+  uint8_t sense_res_raw;
+  int16_t registerValue;
+  Wire.beginTransmission(dsAddr);
+  Wire.write(0x69);
+  Wire.endTransmission();
+  Wire.requestFrom(dsAddr, 1, true);
+  sense_res_raw = Wire.read();
+  Wire.endTransmission();
+  sense_res = 1000 / sense_res_raw;
+  
   Wire.beginTransmission(dsAddr);
   Wire.write(REG_CURRENT_DS);
   Wire.endTransmission();
   Wire.requestFrom(dsAddr, 1, true);
   registerValue = Wire.read();
   Wire.endTransmission();
-  registerValue = (registerValue*1563)/100;//100000
+  registerValue = registerValue*(1563/sense_res);
   return registerValue;
 }
 
